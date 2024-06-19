df76f3f8e560b9c762627f47607ffda4f8578413
AJ <joonh3023203@gmail.com>
2024-06-19 16:17:52 +0800

    重構交易程序

--- decrypt.py
+++ decrypt.py
@@ -1,14 +1,18 @@
 import json
 from eth_account import Account
 
-# 設置 `keystore` 文件路徑和賬戶密碼
-keystore_file_path = r'UTC--2024-05-22T08-23-18.599501038Z--2bdf99f7460156211739b275b9a22f983c011e55'
-password = '0000'
 
-# 讀取 `keystore` 文件內容
-with open(keystore_file_path) as keyfile:
-    encrypted_key = keyfile.read()
-    private_key = Account.decrypt(encrypted_key, password)
-    private_key_hex = private_key.hex()
+# UTC--2024-05-22T08-23-18.599501038Z--2bdf99f7460156211739b275b9a22f983c011e55
+def decrypt_private_key(private_key_file_path: str) -> str:
+    # 設置 `keystore` 文件路徑和賬戶密碼
+    keystore_file_path = rf'{private_key_file_path}'
+    password = '0000'
 
-print("Private Key:", private_key_hex)
+    # 讀取 `keystore` 文件內容
+    with open(keystore_file_path) as keyfile:
+        encrypted_key = keyfile.read()
+        private_key = Account.decrypt(encrypted_key, password)
+        private_key_hex = private_key.hex()
+
+    print("Private Key:", private_key_hex)
+    return private_key
--- origin_transation.py
+++ origin_transation.py
@@ -3,72 +3,75 @@ import json
 import random
 from web3 import Web3
 
-# 定義電子病歷摘要
+from decrypt import decrypt_private_key
+
+class BlockChainProcess:
+    def __init__(self, from_account: str, to_account: str, private_key_path: str) -> None:
+        self._from_account = Web3.to_checksum_address(from_account)
+        self._to_account = Web3.to_checksum_address(to_account)
+        self._private_key = decrypt_private_key(private_key_path)
+
+        self.remote_node_address = 'http://192.168.1.109:8545'
+        self.web3 = Web3(Web3.HTTPProvider(self.remote_node_address))
+
+    def __summary_ehr(self, ehr):
+        # 將摘要轉換為 JSON 字符串並計算 SHA-256 哈希值
+        ehr_summary_json = json.dumps(ehr, sort_keys=True)
+        ehr_hash = hashlib.sha256(ehr_summary_json.encode('utf-8')).hexdigest()
+        print("EHR Hash:", ehr_hash)
+        return ehr_hash
+
+    def send_transaction(self, ehr) -> str:
+        ehr_hash = self.__summary_ehr(ehr)
+        nonce = self.web3.eth.get_transaction_count(self._from_account, 'pending')
+        # nonce += random.randint(1, 1000)  # 增加随机性
+        print("\ndata_hash_toweb3 : ",self.web3.to_hex(text=ehr_hash))
+        print("")
+        # 構建交易
+        transaction = {
+            'from': self._from_account,
+            'to': self._to_account,
+            'value': 0,
+            'gas': 100000,
+            'gasPrice': self.web3.to_wei('50', 'wei'),
+            'nonce': nonce,
+            'data': self.web3.to_hex(text=ehr_hash)
+        }
+
+        # 簽名交易
+        signed_txn = self.web3.eth.account.sign_transaction(transaction, private_key=self._private_key)
+
+        # 發送交易
+        tx_hash = self.web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        print("Transaction hash:", self.web3.to_hex(tx_hash))
+        return self.web3.to_hex(tx_hash)
+    
+    def search_transaction(self, tx_hash):
+        tx = self.web3.eth.get_transaction(tx_hash)
+        transaction_data = tx['input']
+        return transaction_data
+    
+    def verify_transaction(self, transaction_data, ehr):
+        ehr_hash = self.__summary_ehr(ehr)
+        retrieved_hash = str(transaction_data).strip("b'")
+        print("Retrieved hash:", retrieved_hash)
+
+        # 比較原始哈希值和鏈上存儲的哈希值
+        if ehr_hash == retrieved_hash:
+            print("Hash values match, EHR is verified!")
+            return True
+        else:
+            print("Hash values do not match!")
+            return False
+
+# # 定義電子病歷摘要
 ehr_summary = {
     "patient_id": "123456",
     "diagnosis": "Hypertension",
     "prescription": "Medication A, Medication B",
     "date": "2024-05-27"
 }
-
-# 將摘要轉換為 JSON 字符串並計算 SHA-256 哈希值
-ehr_summary_json = json.dumps(ehr_summary, sort_keys=True)
-ehr_hash = hashlib.sha256(ehr_summary_json.encode('utf-8')).hexdigest()
-print("EHR Hash:", ehr_hash)
-
-# 連接到遠程以太坊節點
-remote_node_url = 'http://192.168.1.109:8545'
-web3 = Web3(Web3.HTTPProvider(remote_node_url))
-
-# 檢查連接狀態
-if web3.is_connected():
-    print("Connected to remote Ethereum node")
-
-# 設置賬戶和交易參數
-from_account = '0x2bdf99f7460156211739b275b9a22f983c011e55'
-to_account = '0x465047ba558172c7a8e9999bd2a080e7a0577e91'
-private_key = '0xd7d37d837a297fbe4788cc2ddd52ffe449b1876097737346eed340dfc7e4a54a'
-
-from_account = Web3.to_checksum_address(from_account)
-to_account = Web3.to_checksum_address(to_account)
-
-nonce = web3.eth.get_transaction_count(from_account, 'pending')
-# nonce += random.randint(1, 1000)  # 增加随机性
-print("\ndata_hash_toweb3 : ",web3.to_hex(text=ehr_hash))
-print("")
-# 構建交易
-transaction = {
-    'from': from_account,
-    'to': to_account,
-    'value': 0,
-    'gas': 100000,
-    'gasPrice': web3.to_wei('50', 'wei'),
-    'nonce': nonce,
-    'data': web3.to_hex(text=ehr_hash)
-}
-
-# 簽名交易
-signed_txn = web3.eth.account.sign_transaction(transaction, private_key=private_key)
-
-# 發送交易
-tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-print("Transaction hash:", web3.to_hex(tx_hash))
-
-# 查詢交易回執
-# tx_receipt = web3.eth.get_transaction_receipt(tx_hash)
-# print("Transaction receipt:", tx_receipt)
-
-# 查詢交易數據
-tx = web3.eth.get_transaction(tx_hash)
-data = tx['input']
-
-# 驗證哈希值
-# retrieved_hash = web3.to_text(hexstr=data)
-retrieved_hash = str(data).strip("b'")
-print("Retrieved hash:", retrieved_hash)
-
-# 比較原始哈希值和鏈上存儲的哈希值
-if ehr_hash == retrieved_hash:
-    print("Hash values match, EHR is verified!")
-else:
-    print("Hash values do not match!")
+black_chain_process = BlockChainProcess('0x2bdf99f7460156211739b275b9a22f983c011e55', '0x465047ba558172c7a8e9999bd2a080e7a0577e91', 'UTC--2024-05-22T08-23-18.599501038Z--2bdf99f7460156211739b275b9a22f983c011e55')
+tx_hash = black_chain_process.send_transaction(ehr_summary)
+transaction_data = black_chain_process.search_transaction(tx_hash)
+black_chain_process.verify_transaction(transaction_data, ehr_summary)
