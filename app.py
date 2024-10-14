44798c34b4288c88b27ebcb1943f4779f81b3b84
AJ <joonh3023203@gmail.com>
2024-10-14 14:18:30 +0800

    把search的methods改成get，再把搜尋的部分做字串處理

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
-from origin_transation import BlockChainProcess
+from web3 import Web3
 
+from origin_transation import BlockChainProcess
 app = Flask(__name__)
 
 # 初始化 BlockChainProcess
@@ -14,16 +15,26 @@ block_chain_process = BlockChainProcess(
 def index():
     return render_template('index.html')
 
+# 下面這段與新添加的部分會衝突，執行前請先註解掉或刪除
 @app.route('/send_transaction', methods=['POST'])
 def send_transaction():
     ehr_summary = request.json
     tx_hash = block_chain_process.send_transaction(ehr_summary)
     return jsonify({'transaction_hash': tx_hash})
-
-@app.route('/search_transaction', methods=['POST'])
+# 以上會衝突
+@app.route('/search_transaction', methods=['GET'])
 def search_transaction():
-    tx_hash = request.json['tx_hash']
+    # tx_hash = request.json['tx_hash']
+    # transaction_data = block_chain_process.search_transaction(tx_hash)
+    # return jsonify({'transaction_data': transaction_data})
+
+    tx_hash = request.args.get('tx_hash')
     transaction_data = block_chain_process.search_transaction(tx_hash)
+    transaction_data = str(transaction_data).strip("b'")
+    # if isinstance(transaction_data, dict):
+    #    for key, value in transaction_data.items():
+    #        if isinstance(value, bytes) or isinstance(value, Web3.toHex):
+    #            transaction_data[key] = Web3.toHex(value)
     return jsonify({'transaction_data': transaction_data})
 
 @app.route('/verify_transaction', methods=['POST'])
@@ -32,6 +43,11 @@ def verify_transaction():
     ehr_summary = request.json['ehr_summary']
     is_verified = block_chain_process.verify_transaction(transaction_data, ehr_summary)
     return jsonify({'is_verified': is_verified})
+
+@app.route('/puf_check', methods=['GET'])
+def get_user_id():
+    user_id = request.args.get('user_id')
+    return user_id
 
 if __name__ == '__main__':
     app.run(debug=True)
